6d86d5facd74592bf21dac56d29461dc677b0e8f
MarkZeroni <mark.work1357@gmail.com>
2021-02-01 21:32:45 +0930

    Changed prediction to use the turtle

--- TurtleQuestion/ChristmasTurtle.py
+++ TurtleQuestion/ChristmasTurtle.py
@@ -1,6 +1,8 @@
 import random
 from turtle import *
 
+sides, length, number = 0, 0, 0
+
 def drawPattern(t, x, y, number, sides, length, color):
     '''Draws a radial pattern of shapes around the point (x,y)'''
     t.up()
@@ -18,25 +20,20 @@ def drawPattern(t, x, y, number, sides, length, color):
         t.lt(360 / number)
     t.end_fill()
 
-def predictiveCircle(t, sides, length):
-    '''Draws a circle that shows the maximum extent of the currently configured shape.'''
+def prediction(t, sides, length):
+    '''Finds and returns the radius of the circle needed to fully enclose a pattern.'''
     pos_a = t.pos()
     t.speed(0)
     for i in range(sides // 2):
         t.fd(length)
         t.lt(360 / sides)
     radius = t.distance(pos_a)
-    t.goto(pos_a + (0, -radius))
-    t.seth(0)
-    t.down()
-    t.circle(radius)
-    t.up()
-    t.goto(pos_a)
+    return radius
 
 def drawTree(t, scale, x, y):
     #Calculates the multiplier needed for 1x scale to take up 90% of the screen height. 649 is the true height of the tree.
-    t.speed(10)
     scale = scale * ((t.screen.window_height() * 0.9) / 649)
+    t.speed(10)
 
     #Setup
     t.width(5)
@@ -90,58 +87,76 @@ def drawTree(t, scale, x, y):
     t.end_fill()
 
 def decorate(t, y):
-    while True:
-        t.up()
-        t.speed(0)
-        t.screen.onscreenclick(t.goto)
-        stop = input("Click the screen where you want the ornament to be placed and then input any key, or stop by typing \"stop\": ")
-        if stop == "stop":
-            break
-        t.screen.onscreenclick(None)
-        y.goto(t.pos())
-        
-        sides = getIntInput("number of sides in each shape")
-        length = getIntInput("length of sides")
 
-        predictiveCircle(y, sides, length)
-
-        number = getIntInput("number of shapes")
-
-        #For random colors - so we don't get ugly looking colors
-        colorList = [
+    #For random colors - so we don't get ugly looking colors
+    colorList = [
         "red", "green", "blue", "yellow", "purple", "cyan", "white"
         ]
 
+    while True:
+        t.up()
+        t.speed(0)
+        t.goto(0,0)
+        y.goto(0,0)
+        
+        # Will be used for predefined shapes
+        stop = input("Select a predefined shape with \"p\", stop by typing \"exit\" or press any other key to create a custom shape: ")
+        if stop == "exit":
+            break
+        elif stop == "p":
+            #Add later
+            print("Predefined shapes not yet supported! Enter a custom shape.")
+
         while True:
-            try:
-                color = input("Enter color string (r for random, c for custom): ")
-                if color == "r":
-                    color = colorList[random.randint(0,6)]
-                    break
+            sides = getIntInput("number of sides in each shape")
+            length = getIntInput("length of sides")
+            number = getIntInput("number of shapes")
 
-                elif color == "c":
-                    while True:
-                        print("Enter RGB values (max 255):")
-                        color = (
-                            int(input("Red: ")), int(input("Green: ")), int(input("Blue: "))
-                        )
-                        if (color[0] <= 255) and (color[1] <= 255) and (color[2] <= 255):
-                            break
+            while True:
+                try:
+                    color = input("Enter color string (r for random, c for custom): ")
+                    if color == "r":
+                        color = colorList[random.randint(0,6)]
+                        break
 
-                        else:
-                            print("Invalid color sequence!")
-                    break
+                    elif color == "c":
+                        while True:
+                            print("Enter RGB values (max 255):")
+                            color = (
+                                int(input("Red: ")), int(input("Green: ")), int(input("Blue: "))
+                            )
+                            if (color[0] <= 255) and (color[1] <= 255) and (color[2] <= 255):
+                                break
 
-                else:
-                    t.pencolor(color)
-                    break
+                            else:
+                                print("Invalid color sequence!")
+                        break
 
-            except:
-                print("Please enter a valid color string!")
+                    else:
+                        t.pencolor(color) #Tests if color is valid
+                        break
 
-        t.speed(10)
-        y.clear()
-        drawPattern(t, t.xcor(), t.ycor(), number, sides, length, color)
+                except:
+                    print("Please enter a valid color string!")
+
+            t.screen.onscreenclick(t.goto)
+            radius = max(prediction(y, sides, length), 1) #Max function is used as a crash occurs if the turtle stretch factor is too small
+            t.shape("circle")
+            t.turtlesize((radius/10),(radius/10),1)
+            t.fillcolor(color)
+
+            stop = input("Move the ornament by clicking on the screen.\nType \"stop\" to remake the ornament or type any other key to confirm position: ")
+
+            t.screen.onscreenclick(None)
+            t.shape("turtle")
+            t.turtlesize(1,1,1)
+
+            if stop == "stop":
+                break
+                        
+            t.speed(10)
+            drawPattern(t, t.xcor(), t.ycor(), number, sides, length, color)
+            break
     t.hideturtle()
 
 def getIntInput(message):
